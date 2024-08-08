b55ebd6c08c8dd9e8e55aa3e3512171a41af8153
connyengstrom <connyengstrom@outlook.com>
2024-08-08 05:03:38 +0200

    Fixed enivorment variables

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router'; 
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = 'http://localhost:5000';
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient, private router: Router) {}
 
